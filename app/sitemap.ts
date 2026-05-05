e52545b1097d4b73d1ee548a6f294ff91c1c12ee
vikash <vikashsepat566@gmail.com>
2026-05-05 14:41:31 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://mediora.fit',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mediora.fit/cart',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mediora.fit/checkout',
+      lastModified: new Date(),
+    },
+  ];
+}
